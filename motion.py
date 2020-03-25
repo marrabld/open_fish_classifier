5c9ae1ed99504b860ba164cbbe5d539bbb7d8913
Jason Larke <jason.larke@curtin.edu.au>
2020-03-25 17:32:18 +0800

    Stashing changes to motion.py

--- motion.py
+++ motion.py
@@ -4,6 +4,7 @@ import json
 from itertools import chain
 from random import randint
 from cv2 import cv2
+from scipy.spatial.distance import cdist
 
 def shapeToBox(shape):
     return (int(shape['x']), int(shape['y']), int(shape['width']), int(shape['height']))
@@ -18,33 +19,86 @@ def drawPoints(frame, points):
     for point in points:
         cv2.circle(frame, tuple(point), 4, (0, 0, 255), -1)
 
+def computeDistances(pts1, pts2):
+    assert(len(pts1) == len(pts2))
+    return [ np.linalg.norm(pts2[i] - pts1[i]) for i in range(len(pts1)) ]
+    
+def sampleFeatures(grayscale, bounds):
+    x, y, w, h = bounds
+
+    # TODO: Make these configurable in the ctor
+    featureParams = dict(
+        maxCorners = 10,
+        qualityLevel = 0.02,
+        minDistance = 0.02 * max(w, h)
+    )
+
+    crop = grayscale[y:y+h, x:x+w]
+    corners = cv2.goodFeaturesToTrack(crop, **featureParams)
+
+    # Normalise the points back to the parent image
+    return np.array([ [ x + c[0][0], y + c[0][1] ] for c in corners ], dtype=np.float32)
+
+class Tracker:
+    def __init__(self, bounds, samples):
+        self.samples = samples
+        self.bounds = bounds
+
+        x, y, w, h = bounds
+        sampleCenter = np.mean(self.samples, axis=0)
+        boundsCenter = [ x + (w / 2), y + (h / 2) ]
+
+        self.offset = np.array([ sampleCenter[0] - boundsCenter[0], sampleCenter[1] - boundsCenter[1]], dtype=np.float32)
+
+    def update(self, grayscale, samples, statuses):
+        sampleCenter = np.mean(samples, axis=0)
+        boundsCenter = sampleCenter - self.offset
+
+        self.bounds = (
+            boundsCenter[0] - (self.bounds[2] / 2),
+            boundsCenter[1] - (self.bounds[3] / 2),
+            self.bounds[2],
+            self.bounds[3]
+        )
+
 def sampleFish(grayscale, boundingBox, count, sigma=0.33):
     # Take a crop of the image where the bounding box is located,
-    # then attempt to find edges using the Canny
-    crop = grayscale[boundingBox[1]:boundingBox[1]+boundingBox[3], boundingBox[0]:boundingBox[0]+boundingBox[2]]
-    mean = np.median(crop)
+    # then attempt to find edges using the Canny algorithm
+    x, y, w, h = boundingBox
+    crop = grayscale[y:y+h, x:x+w]
+    mean = np.mean(crop) # TODO: Compare median vs mean results
+
     canny = cv2.Canny(crop, mean * (1 - sigma), mean * (1 + sigma))
     filtered = np.where(canny > 0)
 
     edgeCount = len(filtered[0])
-    samples = [ None ] * min(count, edgeCount)
 
-    for i in range(len(samples)):
+    if (edgeCount < count):
+        return []
+
+    samples = [ 
+        [boundingBox[0] + filtered[1][0], boundingBox[1] + filtered[0][0]],
+        [boundingBox[0] + filtered[1][-1], boundingBox[1] + filtered[0][-1]] 
+    ]
+
+    for i in range(2, count):
         index = randint(0, edgeCount - 1)
-        samples[i] = [ boundingBox[0]+filtered[1][index], boundingBox[1]+filtered[0][index] ]
+        samples.append([ boundingBox[0]+filtered[1][index], boundingBox[1]+filtered[0][index] ])
 
     return samples
-    
 
-video = cv2.VideoCapture(r"E:\Work\data\fish\transfer_1908888_files_fdbb9eb7\A000001_R.avi")
+
+MORPH_KERNEL = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+
+video = cv2.VideoCapture(r"C:\Users\254288b\development\data\fish\transfer_1908888_files_fdbb9eb7\A000001_R.avi")
 
 # seed the background subtraction algorithm
-bgsub = cv2.createBackgroundSubtractorKNN()
-video.set(cv2.CAP_PROP_POS_FRAMES, 4550)
+bgsub = cv2.createBackgroundSubtractorMOG2()
+video.set(cv2.CAP_PROP_POS_FRAMES, 4875)
 
-for i in range(1):
-    _, seedFrame = video.read()
-    bgsub.apply(seedFrame, 1)
+_, colorSeedFrame = video.read()
+bgsub.apply(colorSeedFrame)
+seedFrame = cv2.cvtColor(colorSeedFrame, cv2.COLOR_BGR2GRAY)
 
 boxes = None
 
@@ -64,8 +118,13 @@ video.set(cv2.CAP_PROP_POS_FRAMES, startFrame)
 
 _, colorFrame = video.read()
 frame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
+trackers = [ Tracker(b, sampleFeatures(frame, b)) for b in boxes ]
+lastPoints = np.concatenate(tuple([ t.samples for t in trackers ]))
 
-lastPoints =  np.array(list(chain.from_iterable([ sampleFish(frame, b, 4) for b in boxes ])), dtype=np.float32)
+
+
+
+#lastPoints =  np.array(list(chain.from_iterable([ sampleFish(frame, b, 4) for b in boxes ])), dtype=np.float32)
 lastFrame = None
 writer = cv2.VideoWriter('./motion.avi', cv2.VideoWriter_fourcc(*'XVID'), video.get(cv2.CAP_PROP_FPS), (1920, 1080))
 
@@ -88,17 +147,35 @@ writer = cv2.VideoWriter('./motion.avi', cv2.VideoWriter_fourcc(*'XVID'), video.
 for i in range(200):
     _, colorFrame = video.read()
     frame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
+
+    #mask = cv2.absdiff(seedFrame, frame) # bgsub.apply(colorFrame)
+    ##_, mask = cv2.threshold(mask, 5, 255, cv2.THRESH_BINARY)
+    #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, MORPH_KERNEL)
+#
+    #cv2.imshow('diff', mask)
+    #cv2.waitKey()
     
     if lastFrame is not None:
         points, status, error = cv2.calcOpticalFlowPyrLK(lastFrame, frame, lastPoints, None, **lkParams)
         drawPoints(colorFrame, points)
         lastPoints = points
+
+        pos = 0
+        for tracker in trackers:
+            ns = len(tracker.samples)
+            tracker.update(frame, points[pos:pos+ns], status[pos:pos+ns])
+            pos += ns
+
+            x, y, w, h = tracker.bounds
+            print((x, y, w, h))
+            cv2.rectangle(colorFrame, (int(x), int(y)), (int(x + w), int(y + h)), (0, 0, 255), 2)
+
         #print(status)
         #cv2.imshow('frame', colorFrame)
         writer.write(colorFrame)
+        cv2.imshow('frame', colorFrame)
+        cv2.waitKey()
         
-#
-    key = cv2.waitKey()
     lastFrame = frame
 
 video.release()
