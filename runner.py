692c596c60800b3e14fde70c2f340dd8d46ebdc8
Jason Larke <jason.larke@curtin.edu.au>
2020-03-30 09:39:26 +0800

    Updated track to perform background subtraction and bounding box detection

--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import re
 
 from cv2 import cv2
 from track import track
+from argparse import ArgumentParser
 
 VIDEO_DIRECTORY = r"E:\Work\data\fish\videos"
 BOUNDING_BOXES = r".\bounding-boxes.json"
@@ -11,38 +12,44 @@ BOUNDING_BOXES = r".\bounding-boxes.json"
 def shapeToBox(shape):
     return (int(shape['x']), int(shape['y']), int(shape['width']), int(shape['height']))
 
-metadata = None
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Attempt to track bounding boxes through frames')
+    parser.add_argument('attempt', help='Name of the attempt')
 
-with open(BOUNDING_BOXES, 'r') as fd:
-    metadata = json.load(fd)
+    # argparse terminates the process if parse_args() encounters an error
+    args = parser.parse_args()
+    metadata = None
 
-for meta in metadata:
-    match = re.search(r'^(?P<video>.*?)(?P<frame>\d+)\.\w+$', meta['filename'])
+    with open(BOUNDING_BOXES, 'r') as fd:
+        metadata = json.load(fd)
 
-    if match is None:
-        print('unable to parse filename "%s"'%meta['filename'])
-        continue
+    for meta in metadata:
+        match = re.search(r'^(?P<video>.*?)(?P<frame>\d+)\.\w+$', meta['filename'])
 
-    infile = os.path.join(VIDEO_DIRECTORY, match['video'])
-    startFrame = int(match['frame'])
+        if match is None:
+            print('unable to parse filename "%s"'%meta['filename'])
+            continue
 
-    if not os.path.exists(infile):
-        print('unable to find "%s" on disk'%match['video'])
-        continue
+        infile = os.path.join(VIDEO_DIRECTORY, match['video'])
+        startFrame = int(match['frame'])
 
-    # convert shapes to box tuples
-    boxes = [ shapeToBox(r['shape_attributes']) for r in meta['regions'] ]
+        if not os.path.exists(infile):
+            print('unable to find "%s" on disk'%match['video'])
+            continue
 
-    # initialise the reader
-    video = cv2.VideoCapture(infile)
-    video.set(cv2.CAP_PROP_POS_FRAMES, startFrame)
-    
-    # initialise the writer to match the input video dimensions / FPS
-    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = video.get(cv2.CAP_PROP_FPS)
+        # convert shapes to box tuples
+        boxes = [ shapeToBox(r['shape_attributes']) for r in meta['regions'] ]
 
-    outfile = './mov/out/%s_%s.%d.avi'%(os.path.splitext(match['video'])[0], 'optical-flow', startFrame)
-    writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'XVID'), int(fps / 4), (width, height))
+        # initialise the reader
+        video = cv2.VideoCapture(infile)
+        video.set(cv2.CAP_PROP_POS_FRAMES, startFrame)
+        
+        # initialise the writer to match the input video dimensions / FPS
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
 
-    track(video, writer, boxes, 200)
+        outfile = './mov/out/%s_%s.%d.avi'%(os.path.splitext(match['video'])[0], args.attempt, startFrame)
+        writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'XVID'), int(fps / 4), (width, height))
+
+        track(video, writer, boxes, 200)
--- track.py
+++ track.py
@@ -8,19 +8,7 @@ TEMPLATE_THRESHOLD = 0.6
 MORPH_KERNEL = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
 
 def track(reader, writer, boxes, totalFrames):
-    _, colorFrame = reader.read()
-    frame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
-
-    trackers = [ Tracker(b, sampleFeatures(frame, b)) for b in boxes ]
-    lastPoints = np.concatenate(tuple([ t.samples for t in trackers if t.samples is not None ]))
-    lastFrame = frame
-
-    # Tweak-able LK parameters
-    lkParams = dict(
-        winSize = (15, 15),
-        maxLevel = 2,
-        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
-    )
+    bgsub = cv2.createBackgroundSubtractorKNN()
 
     for _ in range(totalFrames):
         res, colorFrame = reader.read()
@@ -29,93 +17,34 @@ def track(reader, writer, boxes, totalFrames):
             print('ran out of frames')
             break
 
-        frame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
+        fgmask = createMask(colorFrame, bgsub, -1)
+        boxes = computeBoundingBoxes(fgmask)
 
-        points, status, _ = cv2.calcOpticalFlowPyrLK(lastFrame, frame, lastPoints, None, **lkParams)
-        drawPoints(colorFrame, points)
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
+        output = cv2.addWeighted(fgmask, 0.5, colorFrame, 0.5, 0)
+        drawBoundingBoxes(output, boxes)
 
-        pos = 0
-        for tracker in trackers:
-            if tracker.samples is None:
-                continue
+        writer.write(output)
 
-            ns = len(tracker.samples)
-            tracker.update(frame, points[pos:pos+ns], status[pos:pos+ns])
-            pos += ns
+def computeBoundingBoxes(mask, minArea=64.0):
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rectangles = [ cv2.boundingRect(c) for c in contours ]
 
-            x, y, w, h = tracker.bounds
-            cv2.rectangle(colorFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    # get around a 'bug' or 'feature' of groupRectangles where it removes standalone rectangles
+    # (threshold = 0 is a no-op, but threshold = 1 discards rectangles that don't have any sub-rectangles)
+    # https://stackoverflow.com/questions/21421070/opencv-grouprectangles-getting-grouped-and-ungrouped-rectangles
+    #partitioned, _ = cv2.groupRectangles(rectangles + rectangles, 1, 0.5)
+    return [ r for r in rectangles if (r[2] * r[3]) >= minArea ]
 
-        lastPoints = np.concatenate(tuple([ t.samples for t in trackers if t.samples is not None ]))
-        writer.write(colorFrame)
-        
-        lastFrame = frame
+def createMask(image, bgsub, learn):
+    mask = bgsub.apply(image, learn)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, MORPH_KERNEL)
+    mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)[1]
+    return mask
 
-class Tracker:
-    def __init__(self, bounds, samples):
-        self.samples = samples
-        self.bounds = bounds
+def drawBoundingBoxes(frame, boxes):
+    for rect in boxes:
+        cv2.rectangle(frame, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), rrgb(), 2)
 
-    def update(self, grayscale, samples, statuses):
-        # compute vectors
-        assert(len(samples) == len(self.samples))
-        vectors = np.subtract(samples, self.samples)
-        average = np.mean(vectors, axis=0)
-
-        x, y, w, h = self.bounds
-        x = int(x + average[0])
-        y = int(y + average[1])
-
-        # clamp values
-        if x < 0:
-            w = w + x
-            x = 0
-        
-        if y < 0:
-            h = h + y
-            y = 0
-
-        self.bounds = (x, y, w, h)
-
-        # If any points are now outside the new bounding box, resample the features
-        # TODO: Add some sort of threshold as the bounding box size never changes
-        # TODO: Worth preserving the in-bounds samples and just resampling the ones that have been lost?
-        if not all(isInRectangle(pt, self.bounds) for pt in samples):
-            self.samples = sampleFeatures(grayscale, self.bounds)
-        else:
-            self.samples = samples
-
-        return self.samples
-
-def drawPoints(frame, points):
-    for point in points:
-        cv2.circle(frame, tuple(point), 4, (0, 0, 255), -1)
-
-def sampleFeatures(grayscale, bounds):
-    x, y, w, h = bounds
-    
-    # Avoid trying to sample tiny boxes at all
-    if w <= 5 or h <= 5:
-        return None 
-
-    # TODO: Make these configurable 
-    featureParams = dict(
-        maxCorners = 10,
-        qualityLevel = 0.02,
-        minDistance = 0.02 * ((w + h) / 2)
-    )
-
-    crop = grayscale[y:y+h, x:x+w]
-    corners = cv2.goodFeaturesToTrack(crop, **featureParams)
-
-    if corners is None:
-        return None
-
-    # Normalise the points back to the parent image
-    return np.array([ [ x + c[0][0], y + c[0][1] ] for c in corners ], dtype=np.float32)
-
-def isInRectangle(point, rectangle):
-    return point[0] >= rectangle[0] \
-        and point[0] < (rectangle[0] + rectangle[2]) \
-        and point[1] >= rectangle[1] \
-        and point[1] < (rectangle[1] + rectangle[3])
+def rrgb():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
