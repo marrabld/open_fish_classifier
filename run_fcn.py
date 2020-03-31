54a0173944520f12f45e175c36b52a25daee41bc
marrabld <marrabld@gmail.com>
2020-03-31 05:26:25 +0000

    added generator for the fcn

---
+++ run_fcn.py
@@ -0,0 +1,32 @@
+from src.model import fcn
+import tensorflow as tf
+from os.path import join as join
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from generator import Generator
+from src.tools.meta import log as log
+
+import pickle
+
+DATA_DIR = '/home/danm/data/fishml/output'
+
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+mirrored_strategy = tf.distribute.MirroredStrategy()
+with mirrored_strategy.scope():
+    model = fcn.fcn_model(len_classes=9, dropout_rate=0)
+
+    train_datagen = Generator(join(DATA_DIR, 'train'), BATCH_SIZE=16, shuffle_images=True)
+    val_datagen = Generator(join(DATA_DIR, 'val'), BATCH_SIZE=16, shuffle_images=True)
+
+    history = fcn.train(model, train_datagen, val_generator=train_datagen, epochs=500)
+
+save_obj(history, 'fcn_history')
--- src/model/fcn.py
+++ src/model/fcn.py
@@ -4,11 +4,29 @@ from src.tools.meta import log as log
 import os
 
 
-def fcn_model(len_classes=5, dropout_rate=0.2, multi_gpu=True):
+def fcn_model(len_classes=5, dropout_rate=0.2, multi_gpu=False):
     # Input layer
     input = tf.keras.layers.Input(shape=(None, None, 3))
 
     # A convolution block
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=1)(input)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    # A convolution block
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=1)(input)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+        # A convolution block
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=1)(input)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+        # A convolution block
     x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=1)(input)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
@@ -26,7 +44,9 @@ def fcn_model(len_classes=5, dropout_rate=0.2, multi_gpu=True):
     predictions = tf.keras.layers.Activation('softmax')(x)
 
     if multi_gpu:
+        tf.compat.v1.disable_eager_execution()
         log.info('Attempting multi GPU')
+
         _model = tf.keras.Model(inputs=input, outputs=predictions)
         model = multi_gpu_model(_model, gpus=4)
 
@@ -38,21 +58,22 @@ def fcn_model(len_classes=5, dropout_rate=0.2, multi_gpu=True):
 
 
 def train(model, train_generator, val_generator, epochs=50):
+    checkpoint_path = './snapshots'
+    os.makedirs(checkpoint_path, exist_ok=True)
+    model_path = os.path.join(checkpoint_path,
+                              'model_epoch_{epoch:02d}_loss_{loss:.2f}_acc_{accuracy:.2f}_val_loss_{'
+                              'val_loss:.2f}_val_acc_{val_accuracy:.2f}.h5')
+
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0001),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-    checkpoint_path = './snapshots'
-    os.makedirs(checkpoint_path, exist_ok=True)
-    model_path = os.path.join(checkpoint_path,
-                              'model_epoch_{epoch:02d}_loss_{loss:.2f}_acc_{accuracy:.2f}_val_loss_{val_loss:.2f}_val_acc_{val_accuracy:.2f}.h5')
-
-    history = model.fit_generator(generator=train_generator,
-                                  steps_per_epoch=len(train_generator),
-                                  epochs=epochs,
-                                  callbacks=[tf.keras.callbacks.ModelCheckpoint(model_path, monitor='val_loss',
-                                                                                save_best_only=True, verbose=1)],
-                                  validation_data=val_generator,
-                                  validation_steps=len(val_generator))
+    history = model.fit(train_generator,
+                        steps_per_epoch=len(train_generator),
+                        epochs=epochs,
+                        callbacks=[tf.keras.callbacks.ModelCheckpoint(model_path, monitor='val_loss',
+                                                                      save_best_only=True, verbose=1)],
+                        validation_data=val_generator,
+                        validation_steps=len(val_generator))
 
     return history
