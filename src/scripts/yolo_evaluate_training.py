f0ed67a418afaee0330371f20019bf73cce9a53d
Jason Larke <jasonlarke@live.com.au>
2020-05-18 10:46:56 +0800

    Updated evaluate training script to allow for specifying a start epoch and the number of models to evaluate It seems that ImageAI may have an issue with freeing models correctly between evaluations, meaning its in-built "train on whole directory" functionality can OOM the GPU after enough evaluations.

--- src/scripts/yolo_evaluate_training.py
+++ src/scripts/yolo_evaluate_training.py
@@ -1,34 +1,73 @@
 import os
+import sys
+import re
 
 from argparse import ArgumentParser
 
-def is_valid_path(path):
-    return not os.path.splitext(path)[0].endswith('.expected')
+def resolve_epoch_models(root_dir, start, total):
+    pattern = r'^detection_model-ex-0*%d--loss' % start
+    models = []
 
+    with os.scandir(os.path.join(root_dir, 'models')) as scanner:
+        for entry in scanner:
+            if entry.is_file():
+                if len(models) > 0 or (len(models) == 0 and re.match(pattern, entry.name)):
+                    models.append(entry.path)
+
+                    if len(models) == total:
+                        break         
+
+    return models
+     
 def main(args):
+    root_dir = os.path.join('training', args.name)
+
+    if not os.path.isdir(root_dir):
+        sys.stderr.write('error: unable to find training run named "%s"\n' % args.name)
+        return 1
+
+    # default to evaluating all models
+    model_paths = [ os.path.join(root_dir, 'models') ]
+    
+    if args.start_epoch:
+        model_paths = resolve_epoch_models(root_dir, args.start_epoch, args.total_epochs)
+
+        if len(model_paths) == 0:
+            sys.stderr.write('error: unable to find a matching model for epoch "%d"\n' % args.start_epoch)
+            return 1
+
+    print('info: evaluating models')
+
+    # defer the import until now to avoid loading TensorFlow if there's a 
+    # simpler error that's going to break the whole script anyway
     from imageai.Detection.Custom import DetectionModelTrainer
+    import tensorflow as tf
+
+    if not tf.test.is_gpu_available:
+        print('warning: GPU support not available, evaluating on CPU')
 
     trainer = DetectionModelTrainer()
     trainer.setModelTypeAsYOLOv3()
-    trainer.setDataDirectory(data_directory=args.directory)
+    trainer.setDataDirectory(data_directory=os.path.join(root_dir, 'test'))
 
-    trainer.evaluateModel(
-        model_path=args.model_path, 
-        json_path=args.config_path, 
-        iou_threshold=args.iou_threshold, 
-        object_threshold=args.object_threshold, 
-        nms_threshold=args.nms_threshold
-    )
+    for model_path in model_paths:
+        trainer.evaluateModel(
+            model_path=model_path, 
+            json_path=os.path.join(root_dir, 'json', 'detection_config.json'), 
+            iou_threshold=args.iou_threshold, 
+            object_threshold=args.object_threshold, 
+            nms_threshold=args.nms_threshold,
+        )
 
-if __name__ == '__main__':
-    parser = ArgumentParser('yolo_evaluate_training', 'Evaluate a training run against a specific model')
-    parser.add_argument('-m', '--model-path', required=True, help='Path to a trained YOLO model')
-    parser.add_argument('-c', '--config-path', required=True, help='Path to the trained YOLO model configuration')
+if __name__ == '__main__':  
+    parser = ArgumentParser('yolo_evaluate_training', 'Evaluate models from a YOLO training run against the test dataset')
+    
     parser.add_argument('-i', '--iou-threshold', required=False, type=float, help='IoU threshold to use during evaluation', default=0.5)
     parser.add_argument('-o', '--object-threshold', required=False, type=float, help='Object threshold to use during evaluation', default=0.3)
     parser.add_argument('-n', '--nms-threshold', required=False, type=float, help='Non-maximum suppression threshold to use during evaluation', default=0.5)
-    parser.add_argument('-d', '--directory', required=True, help='Path to the root directory containing the test dataset')
+    parser.add_argument('-s', '--start-epoch', required=False, type=int, help='A specific epoch to evaluate the model for', default=None)
+    parser.add_argument('-t', '--total-epochs', required=False, type=int, help='Total epochs to evaluate (only used when --start-epoch is specified)', default=1)
+    parser.add_argument('name', help='Name of the training run to evaluate')
 
     args = parser.parse_args()
     exit(main(args) or 0)
-
